b6709dc4febd537ab824e61ac85b9dec9d734a01
lyle <lyle@reinholzfamily.com>
2025-01-28 16:28:02 -0600

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Lyle Reinholz on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weatherAppTests/weatherAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  weatherAppTests.swift
+//  weatherAppTests
+//
+//  Created by Lyle Reinholz on 1/28/25.
+//
+
+import Testing
+
+struct weatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
